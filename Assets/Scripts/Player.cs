422537d99a38986e9c41f402133a80a95721ef2b
DustinEMiller <dustinmiller032@gmail.com>
2018-01-18 20:51:37 -0600

    player spawn points

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Transform playerSpawnsPoints;
+    public bool respawn;
+
+    private Transform[] spawnPoints;
+    private bool lastToggled = false;
+
+    // Use this for initialization
+    void Start () {
+        spawnPoints = playerSpawnsPoints.GetComponentsInChildren<Transform>();	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (lastToggled != respawn) {
+            Respawn();
+            respawn = false;
+        }	else {
+            lastToggled = respawn;
+        }
+	}
+
+    private void Respawn() {
+        int i = Random.Range(1, spawnPoints.Length);
+        transform.position = spawnPoints[i].transform.position;
+    }
+}
