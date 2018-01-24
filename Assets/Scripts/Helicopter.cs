183ff064fe993e97cda46111f65f68e5e255eff9
DustinEMiller <dustinmiller032@gmail.com>
2018-01-23 20:13:09 -0600

    simple scripts for sound and zooming

---
+++ Assets/Scripts/Helicopter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour {
+    public AudioClip callSound;
+    private bool called = false;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("CallHeli") && !called) {
+            called = true;
+            audioSource.clip = callSound;
+            audioSource.Play();
+        }
+       
+    }
+}
